db1085f2021ba8a3debcbf481c4be6cefdca4e71
WareJosephB <warejosephb@gmail.com>
2018-11-26 17:31:14 +0000

    Switch

--- src/main/java/prizegen/IPrizeChecker.java
+++ src/main/java/prizegen/IPrizeChecker.java
@@ -6,15 +6,24 @@ public interface IPrizeChecker {
 		if (accountNumber.length() != length + 1) {
 			return "Error, string the wrong length.";
 		} else {
-			if (accountNumber.startsWith("A")) {
+//			if (accountNumber.startsWith("A")) {
+//				return "Congratulations! You have won £" + String.valueOf(basePrize) + "!";
+//			} else if (accountNumber.startsWith("B")) {
+//				return "Congratulations! You have won £" + String.valueOf(2 * basePrize) + "!";
+//			} else {
+//				return "Sorry, no prize today.";
+//			}
+			String letter = accountNumber.substring(0, 1);
+			switch (letter) {
+			case "A":
 				return "Congratulations! You have won £" + String.valueOf(basePrize) + "!";
-			} else if (accountNumber.startsWith("B")) {
+			case "B":
 				return "Congratulations! You have won £" + String.valueOf(2 * basePrize) + "!";
-			} else {
+			default:
 				return "Sorry, no prize today.";
 			}
 		}
 	}
-	
+
 	String checkPrize(String accountNumber);
 }
